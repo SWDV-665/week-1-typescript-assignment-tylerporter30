831e6c20a124563cfed8122259fce1511e778008
tylerporter30 <54014903+tylerporter30@users.noreply.github.com>
2021-03-26 22:29:20 -0500

    Add files via upload

---
+++ Week1/grocery.ts
@@ -0,0 +1,17 @@
+class Grocery {
+    name: string;
+    quantity : number;
+    
+    constructor(name: string, quantity: number) {
+      this.name = name;
+      this.quantity = quantity;
+    }
+  }
+  
+let ShoppingList: Grocery[] = [new Grocery("Bread",3), new Grocery("Cheese", 1)];
+
+function GroceryList(grocery: Grocery {
+    return Grocery.name; 
+    return Grocery.quantity
+  }
+
